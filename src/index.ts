af6e44f4bd91a4d263222bd602b370299d41b465
Andrew Ho <andyminhtuanho@gmail.com>
2026-04-12 04:42:09 -0700

    Automate removal of internal service exports and empty models folder from SDK surface

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,3 @@ export { CancelablePromise, CancelError } from './core/CancelablePromise';
 export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
-export { AttachmentsService } from './services/AttachmentsService';
-export { BackupsService } from './services/BackupsService';
-export { ChatsService } from './services/ChatsService';
-export { ContactsService } from './services/ContactsService';
-export { FcmService } from './services/FcmService';
-export { HandlesService } from './services/HandlesService';
-export { IcloudService } from './services/IcloudService';
-export { MacosService } from './services/MacosService';
-export { MessagesService } from './services/MessagesService';
-export { OtherService } from './services/OtherService';
-export { ServerService } from './services/ServerService';
-export { WebService } from './services/WebService';
